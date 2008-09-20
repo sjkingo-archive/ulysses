1ba37a8fda71f4c4e9271e5e60abbc2001b33913
Sam Kingston <sam@sjkwi.com.au>
2008-09-21 00:53:37 +1000

    Extraneous brace

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -6,7 +6,7 @@ void spinlock_am(const unsigned char *bit)
 {
     register unsigned int spins = 0;
     while (!(*bit)) { /* waste some CPU */
-        if ((++spins) > AM_THRESH) { yield();
+        if ((++spins) > AM_THRESH) yield();
     }
 }
 
