7f6958b3ae418755d5e05a943b523f8e275d29ac
Sam Kingston <sam@sjkwi.com.au>
2008-09-23 13:42:28 +1000

    Basic type definitions according to POSIX

---
+++ include/types.h
@@ -0,0 +1,9 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef short   uid_t; /* user id */
+typedef short   gid_t; /* group id */
+typedef int     pid_t; /* process id */
+
+#endif
+
---
+++ include/unistd.h
@@ -0,0 +1,9 @@
+
+#ifndef _UNISTD_H
+#define _UNISTD_H
+
+/* POSIX 2.7.1 says this must be defined here */
+#define NULL ((void *)0);
+
+#endif
+
