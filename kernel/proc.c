bbe8719c4f3873ccf9b29bd39c6fcd589b85c817
Sam Kingston <sam@sjkwi.com.au>
2008-09-23 15:07:23 +1000

    Moving comments to .c

--- kernel/proc.c
+++ kernel/proc.c
@@ -4,6 +4,10 @@
 
 #include <unistd.h>
 
+/* init_proc()
+ *  Initialises the process table. This needs to be called *once* by kmain(),
+ *  and never again.
+ */
 void init_proc(void)
 {
     register struct proc *p;
@@ -18,11 +22,18 @@ void init_proc(void)
     proc[0].uid = -1;
     proc[0].egid = -1;
     proc[0].rgid = -1;
+
     proc[0].sched_q = IDLE_Q;
+    proc[0].ready = 1; /* ready to run */
+
     proc[0].s_ticks_left = SCHED_QUANTUM;
     proc[0].s_quantum_size = SCHED_QUANTUM;
 }
 
+/* get_proc()
+ *  Searches the process table for a process with the given pid. Returns
+ *  a pointer to the process struct if found, or NULL if not.
+ */
 struct proc *get_proc(pid_t pid)
 {
     register struct proc *p;
--- kernel/proc.h
+++ kernel/proc.h
@@ -4,40 +4,35 @@
 
 #include <types.h>
 
-/* An entry in the process table */
+/* An entry in the process table. When updating this, be sure to keep 
+ * init_proc() updated as well.
+ */
 struct proc {
-    pid_t pid;
-    uid_t uid;
+    pid_t pid; /* process id */
+    uid_t uid; /* user id */
     gid_t egid; /* effective group id */
     gid_t rgid; /* real group id */
 
     unsigned short sched_q; /* scheduling queue this proc belongs to */
+    flag_t ready; /* whether this process is ready to be run */
 
     unsigned short s_ticks_left; /* number of scheduling ticks left */
     unsigned short s_quantum_size; /* quantum size in ticks */
 };
+
 #define NR_PROCS 100
 struct proc proc[NR_PROCS]; /* the process table itself */
 
 #define BEG_PROC (&proc[0]) /* pointer to the start of proc table */
 #define END_PROC (&proc[NR_PROCS]) /* pointer to the end of the proc table */ 
 
-/* Core kernel proccess pids */
+/* Core kernel process pids */
 #define PID_NONE -1
 #define PID_IDLE 0
 
 #define IDLE_P get_proc(PID_IDLE)
 
-/* init_proc()
- *  Initialises the process table. This needs to be called *once* by kmain(),
- *  and never again.
- */
 void init_proc(void);
-
-/* get_proc()
- *  Searches the process table for a process with the given pid. Returns
- *  a pointer to the process struct if found, or NULL if not.
- */
 struct proc *get_proc(pid_t pid);
 
 #endif
