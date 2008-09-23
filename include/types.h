76cb338c172a51eff82f15bc26cf385a77df5a5a
Sam Kingston <sam@sjkwi.com.au>
2008-09-23 15:19:48 +1000

    flag_t for boolean 0/1 checks

--- include/types.h
+++ include/types.h
@@ -1,9 +1,10 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-typedef short   uid_t; /* user id */
-typedef short   gid_t; /* group id */
-typedef int     pid_t; /* process id */
+typedef short uid_t; /* user id */
+typedef short gid_t; /* group id */
+typedef int pid_t; /* process id */
+typedef unsigned char flag_t; /* 0/1 logical flag */
 
 #endif
 
