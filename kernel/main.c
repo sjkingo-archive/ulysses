705d7ddb17c6c372ba1261ed2b3bf0069e140210
Sam Kingston <sam@sjkwi.com.au>
2008-09-27 21:39:42 +1000

    Fiddling with order and comments

--- kernel/main.c
+++ kernel/main.c
@@ -2,24 +2,28 @@
 #include "kernel.h"
 #include "proc.h"
 
-void _kmain(void *mdb, unsigned int magic)
+void print_startup(void)
 {
-    /* Initialise screen - this must be the very first thing we do! */
-    init_screen();
-
-    /* Print some pretty startup info */
     kprintf("KOS - Kingo Operating System\n");
     kprintf("version 0.1 (if-it-works-have-a-party)\n\n");
     kprintf("XXX BIOS memory map\n");
+}
 
-    /* Set up the process table and scheduling queues */
+void _kmain(void *mdb, unsigned int magic)
+{
+    /* Do initialisations */
+    init_screen(); /* must be first */
+    print_startup();
+
+    /* Set up the process table and scheduling queues and add IDLE as first
+     * proc in table.
+     */
     init_proc();
-    
-    /* Add IDLE as first proc in table and schedule it */
     new_proc(-1, -1, -1, "IDLE");
     proc[0].pid = PID_IDLE; /* XXX ignore the auto-generated pid */
-    sched(get_proc(PID_IDLE));
 
+    /* Start IDLE going and never return */
+    sched(get_proc(PID_IDLE));
     panic("_kmain() exited?");
 }
 
