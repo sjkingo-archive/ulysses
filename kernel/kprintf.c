97b663bf097e125b8913b57dbbf2428602ce8b3f
Sam Kingston <sam@sjkwi.com.au>
2008-09-30 16:13:00 +1000

    Scroll the screen if y >= HEIGHT

--- kernel/kprintf.c
+++ kernel/kprintf.c
@@ -50,6 +50,30 @@ void move_cursor(const unsigned int x, const unsigned int y)
     outb(0x3d5, (index & 0xFF)); /* data */
 }
 
+/* scroll_screen()
+ *  Move every character in video memory up one line. This is a very slow 
+ *  and inefficient process, so we request GCC to place all variables on
+ *  CPU registers instead of stack.
+ */
+void scroll_screen(void)
+{
+    /* Each char in memory is 2 bytes wide, so we need to deal with both when
+     * moving.
+     */
+    register unsigned short h = HEIGHT * 2;
+    register unsigned short w = WIDTH * 2;
+    register unsigned short last_line = HEIGHT - 1;
+
+    register unsigned short i;
+    for (i = 0; i < (h * w); i++) {
+        screen.mem[i - w] = screen.mem[i]; /* move one line backwards */
+    }
+
+    /* NOTE: do not update x! */
+    screen.next_y = last_line; /* write to the last line */
+    move_cursor(screen.next_x, last_line);
+}
+
 void kputc(const char c)
 {
     unsigned int index;
@@ -59,6 +83,9 @@ void kputc(const char c)
         screen.next_x = 0;
         screen.next_y++;
     }
+
+    /* Overflow the screen if needed */
+    if (screen.next_y >= HEIGHT) scroll_screen();
 
     /* Each character in video mem is 2 bytes */
     index = (screen.next_x * 2) + ((screen.next_y * 2) * WIDTH);
