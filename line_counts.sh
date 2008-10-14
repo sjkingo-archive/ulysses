d15fc320b0e810615ef91c75a51aa9c920dbd971
Sam Kingston <sam@sjkwi.com.au>
2008-10-14 18:34:47 +1000

    Line counting script

---
+++ line_counts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+let total=0
+
+for ext in c h s ; do
+    let count=0
+    for i in `find . -name \*.$ext -exec wc -l {} \; | awk '{ print $1 }'` ; do 
+        count=$(($count+$i))
+    done
+    total=$(($total+$count))
+    echo "*.$ext: $count"
+done
+
+echo "total: $total"
+
