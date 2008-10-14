9dc4f65a8ec1c6d7becaaf46f719cbd6efe08ee8
Sam Kingston <sam@sjkwi.com.au>
2008-10-14 10:29:49 +1000

    Some string-related functions

--- include/string.h
+++ include/string.h
@@ -4,7 +4,22 @@
 
 #include <types.h>
 
+/* memset()
+ *  Fill memory dest with a constant byte c up to len bytes.
+ */
 void memset(void *dest, int c, size_t len);
+
+/* strlen()
+ *  Calculate length of s, excluding the terminating '\0' char.
+ *  Returns the length.
+ */
+size_t strlen(const char *s);
+
+/* strcpy()
+ *  Copy string src into dest. The destination dest must be large enough.
+ *  Return a pointer to start of destination.
+ */
+char *strcpy(char *dest, const char *src);
 
 #endif
 
--- lib/string.c
+++ lib/string.c
@@ -8,3 +8,19 @@ void memset(void *dest, int c, size_t len)
     while (len--) *p++ = c;
 }
 
+size_t strlen(const char *s)
+{
+    const char *str = s; /* we're butchering the pointer */
+    unsigned int count = 0;
+
+    while (*str++) ++count;
+    return count;
+}
+
+char *strcpy(char *dest, const char *src)
+{
+    char *d = dest; /* to retain start */
+    while (*src) *dest++ = *src++;
+    return d;
+}
+
