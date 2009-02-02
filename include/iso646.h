e93e1f0ac34c9102e5f145aca204424f8862e1e9
Sam Kingston <sam@sjkwi.com.au>
2009-02-02 22:30:20 +1000

    iso646.h for alternate spellings

---
+++ include/iso646.h
@@ -0,0 +1,23 @@
+
+#ifndef _ISO646_H
+#define _ISO646_H
+
+/* Alternate spellings of boolean and bitwise operations that are not easy
+ * to type on international and non-QWERTY keyboards, as per SUS:
+ * http://www.opengroup.org/onlinepubs/009695399/
+ */
+
+#define and &&
+#define and_eq &=
+#define bitand &
+#define bitor |
+#define compl ~
+#define not !
+#define not_eq !=
+#define or ||
+#define or_eq |=
+#define xor ^
+#define xor_eq ^=
+
+#endif
+
