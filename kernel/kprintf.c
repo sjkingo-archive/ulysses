ae5ed2f2574edfd425def2ed693700e7d84a69ab
Sam Kingston <sam@sjkwi.com.au>
2009-04-20 19:09:59 +1000

    Finally fix hex-printing code

--- kernel/kprintf.c
+++ kernel/kprintf.c
@@ -54,15 +54,15 @@ static void kputx(const unsigned long hex, const char hex_table[16],
         flag_t all)
 {
     unsigned long h = hex; /* since we are modifying it */
-    char str[16];
-    char *ptr = str;
+    char str[32];
+    char *ptr = str + 32;
 
-    do {
-        *ptr++ = hex_table[(h % 0x10)];
-    } while ((h /= 0x10) > 0);
     *ptr = '\0';
+    do {
+        *--ptr = hex_table[(h % 0x10)];
+    } while ((h /= 0x10) > 0);
 
-    kputs(str, all);
+    kputs(ptr, all);
 }
 
 /* kprint()
