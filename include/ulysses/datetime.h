1e7e8a36a7d8e0e548103cf3658644ea693edacb
Sam Kingston <sam@sjkwi.com.au>
2009-04-20 21:30:57 +1000

    oops, this belongs ages ago

---
+++ include/ulysses/datetime.h
@@ -0,0 +1,13 @@
+#ifndef _DATETIME_H
+#define _DATETIME_H
+
+struct datetime {
+  unsigned int year;
+  unsigned int month;
+  unsigned int day;
+  unsigned int hour;
+  unsigned int min;
+  unsigned int sec;
+};
+
+#endif
