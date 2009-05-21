150fd0f4e00fe1dc4219b7f366722a61723fe2fb
Sam Kingston <sam@sjkwi.com.au>
2009-05-21 18:18:57 +1000

    License preamble for kernel

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,5 +1,23 @@
-/* System call implementation. The dispatcher for this is in arch/X/syscall.c.
+/* syscall.c - system call layer
+ * part of Ulysses, a tiny operating system
  *
+ * Copyright (C) 2008, 2009 Sam Kingston <sam@sjkwi.com.au>
+ * 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * The dispatcher for this is in arch/X/syscall.c.
  * To add a new system call:
  *   1. Add a new definition, macro (optional) and function declaration to
  *      <include/syscall.h>. Make sure you pick a new, unused number for the
