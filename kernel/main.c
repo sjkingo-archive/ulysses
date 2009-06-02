c192d1aae016b7b8984a9fbe8c9599ec10cf81f0
Sam Kingston <sam@sjkwi.com.au>
2009-06-02 15:02:57 +1000

    kernel startup has ms

--- kernel/main.c
+++ kernel/main.c
@@ -84,8 +84,9 @@ void _kmain(void *mdb, unsigned int magic, unsigned int initial_stack)
     /* XXX this is where we would load init process off the initrd */
 
     /* And we're done. */
-    kprintf("Kernel startup complete in %ds\n", 
-            kern.current_time_offset.tv_sec);
+    kprintf("Kernel startup complete in %ds (%dms)\n", 
+            kern.current_time_offset.tv_sec,
+            kern.current_time_offset.tv_msec);
 
     /* This is the kernel task (pid 0), so drop to an idle */
     idle_cpu();
