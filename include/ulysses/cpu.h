49ca23f8e31e169e32c43778c1dba0826f9100b5
Sam Kingston <sam@sjkwi.com.au>
2009-06-13 22:48:17 +1000

    access to the real-time clock

--- include/ulysses/cpu.h
+++ include/ulysses/cpu.h
@@ -1,6 +1,18 @@
 #ifndef _ULYSSES_CPU_H
 #define _ULYSSES_CPU_H
 
+/* cpu_ticks()
+ *  Get the number of CPU ticks since the last processor reset. We do this in
+ *  assembler to be as efficient as possible. Note that some overhead exists in
+ *  merging the two 32-bit registers. Returns an unsigned long long.
+ */
+#define cpu_ticks() \
+({ \
+    unsigned int a, d; \
+    __asm__ __volatile__("rdtsc" : "=a" (a), "=d" (d)); \
+    ((unsigned long long)a | ((unsigned long long)d << 32)); \
+})
+
 /* idle_cpu()
  *  Loop forever in S1 sleep state. Set interrupts before halting the CPU
  *  to ensure the CPU has active interrupt lines.
