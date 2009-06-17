e9fc6b8e8be9070d72335c538e81dd39aa53cf42
Sam Kingston <sam@sjkwi.com.au>
2009-06-17 12:04:06 +1000

    we can't do this

--- include/ulysses/kthread.h
+++ include/ulysses/kthread.h
@@ -55,11 +55,6 @@ void kthreadd(void);
  */
 pid_t kthread_create(void (*func)(void), const char *name);
 
-/* kthread_clone()
- *  Clones the calling kernel thread.
- */
-pid_t kthread_clone(void);
-
 /* kthread_yield()
  *  Voluntarily yield the current kernel thread, allowing the task manager to 
  *  run another task.
