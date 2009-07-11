b9bce9ce536bbd1766913ce165aa1ab06c4b3caa
Sam Kingston <sam@sjkwi.com.au>
2009-07-11 15:25:48 +1000

    remove core dumps

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scons -Q -c
-find . -name \*.orig -exec rm {} \;
+find . -name \*.orig -or -name core -exec rm {} \;
 rm -f boot/*
 rm -f arch/x86/boot/{initrd.img,kernel}
 rm -f arch/x86/boot/boot/grub/menu.lst
