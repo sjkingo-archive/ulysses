d8901a04cdcadce23546a721957a7eb47cdc6ca4
Sam Kingston <sam@sjkwi.com.au>
2009-07-22 08:25:43 +1000

    hrm -or doesn't work

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 scons -Q -c
-find . -name \*.orig -or -name core -exec rm {} \;
+find . -name \*.orig -exec rm {} \;
+find . -name core -exec rm {} \;
 rm -f boot/*
 rm -f arch/x86/boot/{initrd.img,kernel}
 rm -f arch/x86/boot/boot/grub/menu.lst
