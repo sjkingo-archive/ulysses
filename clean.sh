96090fa3eb76eaecbc44c8c58d1cd9f5d7e603bc
Sam Kingston <sam@sjkwi.com.au>
2009-11-13 22:00:01 +1000

    use xargs as it's more efficient

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 scons -Q -c
-find . -name \*.orig -exec rm {} \;
-find . -name core -exec rm {} \;
-find . -name *.o -exec rm {} \;
+find . -name \*.orig -print | xargs rm -f
+find . -name core -print | xargs rm -f
+find . -name *.o -print | xargs rm -f
 rm -f boot/*
 rm -f arch/x86/boot/{initrd.img,kernel}
 rm -f arch/x86/boot/boot/grub/menu.lst
