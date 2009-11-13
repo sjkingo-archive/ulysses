e43696aaa030169b955f9dea44ad5572b58c390c
Sam Kingston <sam@sjkwi.com.au>
2009-11-13 21:59:12 +1000

    remove stale .o's

--- clean.sh
+++ clean.sh
@@ -2,6 +2,7 @@
 scons -Q -c
 find . -name \*.orig -exec rm {} \;
 find . -name core -exec rm {} \;
+find . -name *.o -exec rm {} \;
 rm -f boot/*
 rm -f arch/x86/boot/{initrd.img,kernel}
 rm -f arch/x86/boot/boot/grub/menu.lst
